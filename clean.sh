5fc4be19260ac2e0c3a250b3394022302843b6a9
Larry Peacock <larry@appsmiths.com>
2020-05-24 05:34:16 -0500

    clean.sh

---
+++ clean.sh
@@ -0,0 +1,8 @@
+
+rm -rf /i/ports/build*/flex*
+
+rm -f ${ASV_PLAT_PORTS}/bin/bison++ \
+${ASV_PLAT_PORTS}/lib/bison.cc \
+${ASV_PLAT_PORTS}/lib/bison.h \
+${ASV_PLAT_PORTS}/man/bison++.1 \
+${ASV_PLAT_PORTS}/man/bison++.1.dman
